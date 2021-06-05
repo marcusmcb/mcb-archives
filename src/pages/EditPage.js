20c7b2787dce2160cfd5f98ec38c5bf5b9cb6687
Marcus McBride <marcusmcb@gmail.com>
2021-06-04 20:20:23 -0700

    added base code for edit page

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/box'
+import Grid from '@material-ui/core/Grid'
 
+import Titlebar from '../components/shared/Titlebar'
 import './editpage.css'
 
 const EditPage = () => {
-    return (
-        <div>
-            EDIT PAGE
-        </div>
-    )
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      color: theme.palette.text.secondary,
+    },
+  }))
+
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Titlebar />
+      <Grid>
+          <Grid item xs={12}>
+              <Box bgcolor='text.secondary' className='edit-content-title'>
+                LATEST FROM SOUNDCLOUD:
+              </Box>
+          </Grid>
+          <Grid item xs={12}>
+              
+          </Grid>
+      </Grid>
+    </div>
+  )
 }
 
-export default EditPage;
+export default EditPage
