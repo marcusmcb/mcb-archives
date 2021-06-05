4502faeafd80065e660235609cade5b777e084c3
Marcus McBride <marcusmcb@gmail.com>
2021-06-05 12:27:14 -0700

    fixed typo in titlebar

--- src/components/shared/Titlebar.js
+++ src/components/shared/Titlebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Box from '@material-ui/core/box'
+import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import { NavLink } from 'react-router-dom'
 
