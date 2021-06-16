a878f0cada42e784c01db54f69c826eb70910576
Marcus McBride <marcusmcb@gmail.com>
2021-06-16 14:43:39 -0700

    added amp config for graphql access

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Amplify from '@aws-amplify/core'
+import { API } from 'aws-amplify'
+import { listArchiveItems } from './graphql/queries'
 
 import MainPage from './pages/MainPage'
 import EditPage from './pages/EditPage'
 import MixshowPage from './pages/MixshowPage'
 
-import './App.css';
+import awsExports from './aws-exports'
+
+import './App.css'
+
+Amplify.configure(awsExports)
 
 function App() {
-  
+  const [archiveItems, setArchiveItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        let apiData = await API.graphql({ query: listArchiveItems })
+        return apiData
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchItems().then((data) => {
+      setArchiveItems(data)
+      console.log(archiveItems)
+    })
+  }, [])
+
   // define page routes
   const routes = (
     <Switch>
       <Route path='/' exact>
-        <MainPage/>
+        <MainPage />
       </Route>
       <Route path='/edits'>
-        <EditPage/>
+        <EditPage />
       </Route>
       <Route path='/mixshows'>
-        <MixshowPage/>
+        <MixshowPage />
       </Route>
     </Switch>
-  ) 
-  
+  )
+
   return (
     <Router>
       <div className='App'>{routes}</div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
