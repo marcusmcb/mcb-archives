46f9c96d1fbd5b88edc14315f7c19b638ab6d4f6
Marcus McBride <marcusmcb@gmail.com>
2021-06-30 09:46:30 -0700

    added main page text and basic css

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,12 +5,15 @@ import Titlebar from '../components/shared/Titlebar'
 import './mainpage.css'
 
 const MainPage = () => {
-    return (
-        <div>
-            <Titlebar/>
-            MAIN PAGE CONTENT
-        </div>
-    )
+  return (
+    <div>
+      <Titlebar />
+      <div className='main-page-text'>
+        <p>Welcome to my archive site!</p>
+        <p>I've been producing remixes, mashups, and DJ edits for my own sets and mixshows since the 90s.  Here, you'll find all the stuff that I've released over the years.</p>
+      </div>    
+    </div>
+  )
 }
 
-export default MainPage;
+export default MainPage
